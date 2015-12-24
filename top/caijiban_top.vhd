5213d3d61d1d7892719568923a90c67ab4783d1f
jlong299 <jlong299@foxmail.com>
2015-12-24 17:23:21 +0800

    Add RSSI and LED
    RSSI_p4 : parallel 8 RSSI calculator
    LED_RSSI_blink :   two LEDs.    weak : 0+0   normal : blink + 0
    strong : blink + 1     overflow : 1+1
    LED pins loca : D2 AL24,  D3 AM23,  D4 AG21, D5 AE22, D6 AM24,  D7 AF21
    Now use D5 + D2

--- top/caijiban_top.vhd
+++ top/caijiban_top.vhd
@@ -8,12 +8,12 @@ use ieee.numeric_std.all;
 
 
 entity caijiban_top is
-port	
+port
 (
  --dac interface
    dac_dataclko : in  std_logic                     ;
    --dac_dataclki : out std_logic                     ;
-   dac_data     : out std_logic_vector(14 downto 0) ;  
+   dac_data     : out std_logic_vector(14 downto 0) ;
    dac_reset    : out std_logic                     ;
    dac_irq      : out std_logic                     ;
    dac_cs       : out std_logic                     ;
@@ -21,9 +21,9 @@ port
    dac_sclk     : out std_logic                     ;
    dac_sdo      : out std_logic                     ;
    dac_mode     : out std_logic                     ;
-	
- 
- --adc interface 
+
+
+ --adc interface
    adc_fsr      : out std_logic                     ;
    adc_ddrph    : out std_logic                     ;
    adc_des      : out std_logic                     ;
@@ -37,16 +37,17 @@ port
    adc_dclkq    : in  std_logic                     ;
    adc_di       : in  std_logic_vector(11 downto 0) ;
    adc_dq       : in  std_logic_vector(11 downto 0) ;
-	
+
 	clk_toFPGA2  : out std_logic;
 	d_fromFPGA2	 : in std_logic_vector(5 downto 0); -- ddio_in format  (5) clk (4) valid  (3..0) data
-	d_toFPGA2	 : out std_logic_vector(5 downto 0) --(5)clk  (4)valid
- 
- 
+	d_toFPGA2	 : out std_logic_vector(5 downto 0); --(5)clk  (4)valid
+  LED_out : out std_logic_vector(1 downto 0)  -- D5,D2
+
+
 );
 end entity caijiban_top ;
-	
-architecture rtl of caijiban_top is 
+
+architecture rtl of caijiban_top is
 
 component adin is
 port(
@@ -63,29 +64,29 @@ port(
    --data clk interface
    adc_dclki          :  in std_logic                     ;
    adc_dclkq          :  in std_logic                     ;
-   adc_di             :  in std_logic_vector(11 downto 0) ; 
+   adc_di             :  in std_logic_vector(11 downto 0) ;
    adc_dq             :  in std_logic_vector(11 downto 0) ;
    --adc usr interface
-   adc_d0             : out std_logic_vector(11 downto 0) ;   
+   adc_d0             : out std_logic_vector(11 downto 0) ;
    adc_d1             : out std_logic_vector(11 downto 0) ;
    adc_d2             : out std_logic_vector(11 downto 0) ;
-   adc_d3             : out std_logic_vector(11 downto 0) ;   
-   adc_d4             : out std_logic_vector(11 downto 0) ;   
+   adc_d3             : out std_logic_vector(11 downto 0) ;
+   adc_d4             : out std_logic_vector(11 downto 0) ;
    adc_d5             : out std_logic_vector(11 downto 0) ;
    adc_d6             : out std_logic_vector(11 downto 0) ;
-   adc_d7             : out std_logic_vector(11 downto 0) ;     		
-   data_rx_outclock   : out std_logic        	
-); 
+   adc_d7             : out std_logic_vector(11 downto 0) ;
+   data_rx_outclock   : out std_logic
+);
 end component ;
-	
-	
+
+
 component dacout is
 port(
  -- usr interface
   dac_usr_clk    : out std_logic                      ;
   dac_usr_data   : in  std_logic_vector(111 downto 0) ;
   use_test_data  : in  std_logic                      ;
- 
+
  -- dac chip interface
  dac_dataclko  : in  std_logic                     ;
  --dac_dataclki  : out std_logic                     ;
@@ -97,11 +98,11 @@ port(
  dac_sclk      : out std_logic                     ;
  dac_sdo       : out std_logic                     ;
  dac_mode      : out std_logic      ;
- clk_toFPGA2	  : out std_logic--20MHz 
-);                                     
+ clk_toFPGA2	  : out std_logic--20MHz
+);
 end component ;
- 
- 
+
+
 component MOD_QPSK_16QAM is
 generic(kOutSize : positive := 14);
 port(
@@ -119,7 +120,7 @@ port(
 		ddio_wren : in std_logic;
 		d_src_is_GE : in std_logic;
 		with_LDPC 	: in std_logic;
-	
+
 		IFData_0	: OUT std_logic_vector(kOutSize - 1 downto 0);
 		IFData_1	: OUT std_logic_vector(kOutSize - 1 downto 0);
 		IFData_2	: OUT std_logic_vector(kOutSize - 1 downto 0);
@@ -130,18 +131,18 @@ port(
 		IFData_7  : OUT std_logic_vector(kOutSize - 1 downto 0)
 		);
 end component;
-	
+
 component	Demod_Para	is
 generic	(
 			kInSize  : positive :=8
 		);
 port	(
-		aReset          : in  std_logic; 
+		aReset          : in  std_logic;
 		clk_100         : in  std_logic;
 		pclk_I			: in  std_logic;  -- 50MHz
 		rclk           : in std_logic ; --75MHZ
-		rclk_half		: in std_logic; -- 37.5MHz		
-		
+		rclk_half		: in std_logic; -- 37.5MHz
+
 		AD_d0			: in std_logic_vector(kInSize-1 downto 0);
 		AD_d1			: in std_logic_vector(kInSize-1 downto 0);
 		AD_d2			: in std_logic_vector(kInSize-1 downto 0);
@@ -152,41 +153,42 @@ port	(
 		AD_d7           : in std_logic_vector(kInSize-1 downto 0);
 
 		with_LDPC		: in std_logic;
-		
+
 		--err_test 	: out std_logic;
 		--d_toFPGA2	 : out std_logic_vector(5 downto 0)
 		dat_mux : out std_logic_vector(3 downto 0) ;
-		val_mux : out std_logic
-		
+		val_mux : out std_logic;
+    LED_out : out std_logic_vector(1 downto 0)
+
 		);
-end component;		
-	
-	
-component cpm is 
+end component;
+
+
+component cpm is
 port
 (
 	adc_if_clk_out : in  std_logic ; -- 100MHZ
 	adc_demo_clk   : out std_logic ; -- 100MHZ
 	adc_demo_p_clk : out std_logic ; -- 50MHZ
-	adc_demo_r_clk : out std_logic ; -- 75MHZ	
+	adc_demo_r_clk : out std_logic ; -- 75MHZ
 	adc_demo_r_clk_half : out std_logic ; -- 37.5MHz
-	adc_demo_rst   : out std_logic ; 
+	adc_demo_rst   : out std_logic ;
 	dac_if_clk_out : in  std_logic ; -- 150MHZ
 	dac_out_clk    : out std_logic ; -- 150MHZ
 	dac_source_clk : out std_logic ; -- 50MHZ
 	dac_source_clk_25 : out std_logic; -- 25MHz, 2015/3/4 for LDPC Encoder Source Self-Gen
    dac_source_clk_100 : out std_logic;
-	dac_source_rst : out std_logic 
+	dac_source_rst : out std_logic
 );
 end component;
-	
+
 
 component rom_DAsin IS
 	PORT
 	(
 		result		: OUT STD_LOGIC_VECTOR (0 DOWNTO 0)
 	);
-END component;	
+END component;
 
 component cnst_ifLDPC IS
 	PORT
@@ -217,24 +219,24 @@ component ddioin_buffer is
   port (
   	aReset	: in std_logic;
   	ddio_clk	: in std_logic;  --100MHz
-  	ddio_din	: in std_logic_vector(4 downto 0);  
+  	ddio_din	: in std_logic_vector(4 downto 0);
   	rdclk		: in std_logic;  --100MHz
 
-  	d_out 		: out std_logic_vector(4 downto 0) 
-	
+  	d_out 		: out std_logic_vector(4 downto 0)
+
   ) ;
-end component ;   
+end component ;
 
 
   signal   dac_usr_clk    : std_logic                      ;
   signal   dac_usr_data   : std_logic_vector(111 downto 0) ;
   signal   use_test_data  : std_logic                      ;
- 
+
 	signal  dac_if_clk_out  :  std_logic                     ; -- 150MHZ
 	signal  dac_out_clk     :  std_logic                     ; -- 150MHZ
 	signal  dac_source_clk  :  std_logic                     ; -- 50MHZ
 	signal  dac_source_rst  :  std_logic                     ;
-	
+
 	signal  dac_inp0_data     : std_logic_vector(13 downto 0) ;
 	signal  dac_inp1_data     : std_logic_vector(13 downto 0) ;
 	signal  dac_inp2_data     : std_logic_vector(13 downto 0) ;
@@ -243,14 +245,14 @@ end component ;
 	signal  dac_inp5_data     : std_logic_vector(13 downto 0) ;
 	signal  dac_inp6_data     : std_logic_vector(13 downto 0) ;
 	signal  dac_inp7_data     : std_logic_vector(13 downto 0) ;
-		
-  -- adc signal 
+
+  -- adc signal
   signal  data_rx_outclock  : std_logic        	            ;
   signal 	adc_demo_clk      : std_logic                     ;
 	signal  adc_demo_p_clk    : std_logic                     ;
 	signal  adc_demo_rst      : std_logic                     ;
   signal adc_demo_r_clk : std_logic ;
-    
+
   signal demo_err_test      : std_logic                     ;
   signal adc_d0			        : std_logic_vector(11 downto 0) ;
 	signal adc_d1			        : std_logic_vector(11 downto 0) ;
@@ -259,9 +261,9 @@ end component ;
 	signal adc_d4			        : std_logic_vector(11 downto 0) ;
 	signal adc_d5             : std_logic_vector(11 downto 0) ;
 	signal adc_d6             : std_logic_vector(11 downto 0) ;
-	signal adc_d7             : std_logic_vector(11 downto 0) ;      
-		
-		
+	signal adc_d7             : std_logic_vector(11 downto 0) ;
+
+
 	signal demo_adc_d0        : std_logic_vector(7 downto 0) ;
   signal demo_adc_d1        : std_logic_vector(7 downto 0) ;
   signal demo_adc_d2        : std_logic_vector(7 downto 0) ;
@@ -270,35 +272,35 @@ end component ;
   signal demo_adc_d5        : std_logic_vector(7 downto 0) ;
   signal demo_adc_d6        : std_logic_vector(7 downto 0) ;
   signal demo_adc_d7        : std_logic_vector(7 downto 0) ;
-  
+
   signal dac_source_clk_25, adc_demo_r_clk_half : std_logic;
   signal d_src_is_GE, with_LDPC : std_logic;
 
-  signal data_in_l : std_logic_vector(4 downto 0) ;   
+  signal data_in_l : std_logic_vector(4 downto 0) ;
 
   signal dat_mux : std_logic_vector(3 downto 0) ;
-  signal val_mux : std_logic;    
-									
+  signal val_mux : std_logic;
+
   signal dac_source_clk_100 : std_logic;
   signal dout_ddioin_buffer : std_logic_vector(4 downto 0) ;
 
-	
+
 begin
- 	
-rom_DAsin_inst: rom_DAsin 
+
+rom_DAsin_inst: rom_DAsin
 	PORT map
 	(
 		result(0)		=> use_test_data
 	);
-	
-rom_dsource_inst: rom_dsource 
+
+rom_dsource_inst: rom_dsource
 	PORT map
 	(
 		result(0)		=> d_src_is_GE
 	);
 
 
-cnst_ifLDPC_inst: cnst_ifLDPC 
+cnst_ifLDPC_inst: cnst_ifLDPC
 	PORT map
 	(
 		result(0) => with_LDPC
@@ -306,21 +308,21 @@ cnst_ifLDPC_inst: cnst_ifLDPC
 
 
 -- inst cpm
-inst_cpm: cpm 
+inst_cpm: cpm
 port map
 (
 	adc_if_clk_out => data_rx_outclock ,  -- 100MHZ
 	adc_demo_clk   => adc_demo_clk     ,  -- 100MHZ
 	adc_demo_p_clk => adc_demo_p_clk   ,  -- 50MHZ
 	adc_demo_r_clk => adc_demo_r_clk   , --75MHZ
-	adc_demo_r_clk_half => adc_demo_r_clk_half, -- 37.5MHz	
+	adc_demo_r_clk_half => adc_demo_r_clk_half, -- 37.5MHz
 	adc_demo_rst   => adc_demo_rst     ,
 	dac_if_clk_out => dac_if_clk_out   ,  -- 150MHZ
 	dac_out_clk    => dac_out_clk      ,  -- 150MHZ
 	dac_source_clk => dac_source_clk   ,  -- 50MHZ
 	dac_source_clk_25 => dac_source_clk_25,  -- 25MHz, 2015/3/4 for LDPC Encoder Source Self-Gen
     dac_source_clk_100 => dac_source_clk_100,
-	dac_source_rst => dac_source_rst   
+	dac_source_rst => dac_source_rst
 );
 
 
@@ -347,7 +349,7 @@ port map
 );
 
 
---ddio_fromF2_inst: ddio_fromF2 
+--ddio_fromF2_inst: ddio_fromF2
 --	PORT map
 --	(
 --		aclr		=> dac_source_rst,
@@ -365,11 +367,11 @@ ddioin_buffer_inst: ddioin_buffer
   	rdclk		=> dac_source_clk_100,
 
   	d_out 		=> dout_ddioin_buffer
-	
+
   ) ;
 
 
-dac_mod_inst: MOD_QPSK_16QAM 
+dac_mod_inst: MOD_QPSK_16QAM
 generic map
   (kOutSize => 14)
 port map
@@ -386,15 +388,15 @@ port map
 		with_LDPC => with_LDPC,
 		IFData_0	=> dac_inp0_data  ,
 		IFData_1	=> dac_inp1_data  ,
-		IFData_2	=> dac_inp2_data  , 
+		IFData_2	=> dac_inp2_data  ,
 		IFData_3  => dac_inp3_data  ,
 		IFData_4	=> dac_inp4_data  ,
-		IFData_5  => dac_inp5_data  , 
+		IFData_5  => dac_inp5_data  ,
 		IFData_6	=> dac_inp6_data  ,
-		IFData_7  => dac_inp7_data  
+		IFData_7  => dac_inp7_data
 		);
-		
-							
+
+
 	dac_usr_data	<= (dac_inp0_data(13)) & dac_inp0_data(12 downto 0) &
              	     (dac_inp1_data(13)) & dac_inp1_data(12 downto 0) &
 							     (dac_inp2_data(13)) & dac_inp2_data(12 downto 0) &
@@ -402,13 +404,13 @@ port map
 							     (dac_inp4_data(13)) & dac_inp4_data(12 downto 0) &
 							     (dac_inp5_data(13)) & dac_inp5_data(12 downto 0) &
 							     (dac_inp6_data(13)) & dac_inp6_data(12 downto 0) &
-							     (dac_inp7_data(13)) & dac_inp7_data(12 downto 0);		
-							     	
-							     	
-							     	
+							     (dac_inp7_data(13)) & dac_inp7_data(12 downto 0);
+
+
+
 
  -- inst adc moduel
-adcin_inst: adin 
+adcin_inst: adin
 port map
 (
  --control interface
@@ -426,43 +428,43 @@ port map
  adc_dclkq        => adc_dclkq        ,
  adc_di           => adc_di           ,
  adc_dq           => adc_dq           ,
- --adc usr interface   
- adc_d0           => adc_d0           ,     
- adc_d1           => adc_d1           ,     
- adc_d2           => adc_d2           ,     
- adc_d3           => adc_d3           ,     
- adc_d4           => adc_d4           ,     
- adc_d5           => adc_d5           ,     
- adc_d6           => adc_d6           ,     
- adc_d7           => adc_d7           ,    
- data_rx_outclock => data_rx_outclock 
+ --adc usr interface
+ adc_d0           => adc_d0           ,
+ adc_d1           => adc_d1           ,
+ adc_d2           => adc_d2           ,
+ adc_d3           => adc_d3           ,
+ adc_d4           => adc_d4           ,
+ adc_d5           => adc_d5           ,
+ adc_d6           => adc_d6           ,
+ adc_d7           => adc_d7           ,
+ data_rx_outclock => data_rx_outclock
 
 );
-	
--- adc output is binary offset format	
+
+-- adc output is binary offset format
 -- convert binary offset data to two complete format
 demo_adc_d0 <= (not adc_d0(11)) & adc_d0(10 downto 4) ;
 demo_adc_d1 <= (not adc_d1(11)) & adc_d1(10 downto 4) ;
 demo_adc_d2 <= (not adc_d2(11)) & adc_d2(10 downto 4) ;
-demo_adc_d3 <= (not adc_d3(11)) & adc_d3(10 downto 4) ;	
+demo_adc_d3 <= (not adc_d3(11)) & adc_d3(10 downto 4) ;
 demo_adc_d4 <= (not adc_d4(11)) & adc_d4(10 downto 4) ;
 demo_adc_d5 <= (not adc_d5(11)) & adc_d5(10 downto 4) ;
-demo_adc_d6 <= (not adc_d6(11)) & adc_d6(10 downto 4) ;					
+demo_adc_d6 <= (not adc_d6(11)) & adc_d6(10 downto 4) ;
 demo_adc_d7 <= (not adc_d7(11)) & adc_d7(10 downto 4) ;
-	
-demo_inst:	Demod_Para	
+
+demo_inst:	Demod_Para
 generic	map
    (
 			kInSize  => 8
 		)
 port map
-	(              
+	(
 		aReset    => adc_demo_rst        ,
 		clk_100   => adc_demo_clk        ,
-		pclk_I		=> adc_demo_p_clk      ,  
+		pclk_I		=> adc_demo_p_clk      ,
 		rclk       => adc_demo_r_clk ,
 		rclk_half		=> adc_demo_r_clk_half,
-		
+
 		AD_d0			=> demo_adc_d0 ,
 		AD_d1			=> demo_adc_d1 ,
 		AD_d2			=> demo_adc_d2 ,
@@ -473,15 +475,16 @@ port map
 		AD_d7     => demo_adc_d7 ,
 
 		with_LDPC => with_LDPC,
-		
+
 		--err_test 	=> open,--demo_err_test,
 		--d_toFPGA2	 : out std_logic_vector(5 downto 0)
 		dat_mux => dat_mux,
-		val_mux => val_mux
-		
-		);	
+		val_mux => val_mux,
+    LED_out => LED_out
 
-	ddio_toF2_inst: ddio_toF2 
+		);
+
+	ddio_toF2_inst: ddio_toF2
 	PORT map
 	(
 		aclr		=> adc_demo_rst,
@@ -490,7 +493,7 @@ port map
 		outclock		=> adc_demo_r_clk,
 		dataout		=> d_toFPGA2
 	);
-		
-		
+
+
 
 end architecture rtl ;
